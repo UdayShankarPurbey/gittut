0c607d62537be5c07523f8645264d473afd91072
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-03 14:37:27 +0530

    add data in dataView

---
+++ dataView.js
@@ -0,0 +1,138 @@
+const data = [
+    {
+      "id": 1,
+      "name": "John",
+      "age": 25,
+      "isVoterId": true,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": true,
+      "accessibility": true
+    },
+    {
+      "id": 2,
+      "name": "Alice",
+      "age": 22,
+      "isVoterId": false,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": true,
+      "accessibility": false
+    },
+    {
+      "id": 3,
+      "name": "Bob",
+      "age": 30,
+      "isVoterId": true,
+      "isMatriculationComplete": false,
+      "isIntermediateComplete": true,
+      "accessibility": true
+    },
+    {
+      "id": 4,
+      "name": "Emily",
+      "age": 28,
+      "isVoterId": true,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": false,
+      "accessibility": false
+    },
+    {
+      "id": 5,
+      "name": "Michael",
+      "age": 26,
+      "isVoterId": false,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": false,
+      "accessibility": true
+    },
+    {
+      "id": 6,
+      "name": "Sophia",
+      "age": 24,
+      "isVoterId": true,
+      "isMatriculationComplete": false,
+      "isIntermediateComplete": false,
+      "accessibility": false
+    },
+    {
+      "id": 7,
+      "name": "William",
+      "age": 29,
+      "isVoterId": false,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": true,
+      "accessibility": true
+    },
+    {
+      "id": 8,
+      "name": "Olivia",
+      "age": 23,
+      "isVoterId": true,
+      "isMatriculationComplete": false,
+      "isIntermediateComplete": true,
+      "accessibility": false
+    },
+    {
+      "id": 9,
+      "name": "James",
+      "age": 27,
+      "isVoterId": true,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": false,
+      "accessibility": true
+    },
+    {
+      "id": 10,
+      "name": "Emma",
+      "age": 21,
+      "isVoterId": false,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": false,
+      "accessibility": false
+    },
+    {
+      "id": 11,
+      "name": "Alexander",
+      "age": 31,
+      "isVoterId": true,
+      "isMatriculationComplete": false,
+      "isIntermediateComplete": false,
+      "accessibility": true
+    },
+    {
+      "id": 12,
+      "name": "Ava",
+      "age": 20,
+      "isVoterId": true,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": true,
+      "accessibility": false
+    },
+    {
+      "id": 13,
+      "name": "Daniel",
+      "age": 32,
+      "isVoterId": false,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": true,
+      "accessibility": true
+    },
+    {
+      "id": 14,
+      "name": "Mia",
+      "age": 19,
+      "isVoterId": true,
+      "isMatriculationComplete": false,
+      "isIntermediateComplete": true,
+      "accessibility": false
+    },
+    {
+      "id": 15,
+      "name": "Matthew",
+      "age": 33,
+      "isVoterId": true,
+      "isMatriculationComplete": true,
+      "isIntermediateComplete": false,
+      "accessibility": true
+    }
+  ]
+  
